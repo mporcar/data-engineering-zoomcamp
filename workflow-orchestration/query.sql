983c3c7e15bcffb2079afa4a7c7ec764deaac3fb
mporcar <josepmporcar@gmail.com>
2026-02-05 17:46:35 +0000

    BigQuery

--- workflow-orchestration/query.sql
+++ workflow-orchestration/query.sql
@@ -13,4 +13,4 @@ where extract(year from lpep_pickup_datetime) = 2020;
 #5
 select count(1)
 from public.green_tripdata
-where filename = "yellow_tripdata_2021-04.csv";
+where filename = "yellow_tripdata_2021-03.csv";
